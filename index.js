b56423d57822a653d5ee1baef6f0f21c86a40989
edwin <you@example.com>
2024-01-19 19:35:12 +0200

    Frontend local server finished

--- index.js
+++ index.js
@@ -7,69 +7,8 @@ const port = 3000;
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-    
-})
-
-// $('#saveButton').click(function() {
-//     var lesson = $('#lessonInput').val();
-//     var description = $('#descriptionInput').val();
-
-//     $('#modalLesson').text(lesson);
-//     $('#modalDescription').text(description);
-// });
-
-// $('.modal-footer .btn-primary').click(function() {
-//     const alertBox = $('#lottieAlertSubmited');
-
-//     // Clear previous animation and set up new animation
-//     alertBox.empty().show();
-//     var animation = lottie.loadAnimation({
-//         container: alertBox[0],
-//         renderer: 'svg',
-//         loop: false,
-//         autoplay: true,
-//         path: './animations/Animation-Submited.json'
-//     });
-
-//     // Hide the alert on animation complete
-//     animation.addEventListener('complete', function() {
-//         alertBox.hide();
-//     });
-
-//     // Hide the modal
-//     $('#exampleModal').modal('hide');
-// });
 
 app.listen(port, () => {
     console.log(`server lisening on port ${port}`);
 })
 
-//! In the future. If or else. if the home work not submited or it have an error an Error animation will pop.
-/*
-//* Event listener for the Save changes button in the modal
-$(document).ready(function() {
-    $('.modal-footer .btn-primary').click(function() {
-        const alertBox = $('#lottieAlertError');
-
-        // Clear previous animation and set up new animation
-        alertBox.empty().show();
-        var animation = lottie.loadAnimation({
-            container: alertBox[0], // Direct DOM element reference is needed here
-            renderer: 'svg',
-            loop: false,
-            autoplay: true,
-            path: './animations/Animation-Error.json'
-        });
-
-        // Hide the alert on animation complete
-        animation.addEventListener('complete', function() {
-            alertBox.hide();
-        });
-
-        // Hide the modal
-        $('#exampleModal').modal('hide');
-    });
-
-});
-*/
